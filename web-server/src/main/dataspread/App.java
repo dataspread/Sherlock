45b13d66e84d138b552565e93fa944f48c607d06
Lawrence Chen <lawrencechen2002@gmail.com>
2022-02-23 00:05:46 -0800

    remove cors configuration

--- web-server/src/main/dataspread/App.java
+++ web-server/src/main/dataspread/App.java
@@ -22,27 +22,6 @@ public class App {
 
     port(4567);
 
-    before((Filter) (request, response) -> {
-      response.header("Access-Control-Allow-Origin", "*");
-      response.header("Access-Control-Allow-Headers", "*");
-      response.header("Access-Control-Allow-Methods", "*");
-    });
-
-    options("/*", (request, response) -> {
-
-      String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-      if (accessControlRequestHeaders != null) {
-        response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-      }
-
-      String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-      if (accessControlRequestMethod != null) {
-        response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-      }
-
-      return "OK";
-    });
-
     after((Filter) (request, response) -> {
       response.header("Access-Control-Allow-Origin", "*");
       response.header("Access-Control-Allow-Methods", "*");
